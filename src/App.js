008c34d42ebbd033712d15c230e0cc9257428515
NicolasAndrehh <nicolasolaya22@gmail.com>
2023-07-11 12:52:31 -0500

    Create greeting component to display greeting

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Greeting from './components/Greeting';
 
 function App() {
   return (
-    <div className="App" />
+    <Routes>
+      <Route path="/" element={<h1>Home</h1>} />
+      <Route path="/greeting" element={<Greeting />} />
+    </Routes>
   );
 }
 
---
+++ src/components/Greeting.jsx
@@ -0,0 +1,21 @@
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchGreeting } from '../redux/greetingSlice';
+
+const Greeting = () => {
+  const dispatch = useDispatch();
+  const greeting = useSelector((state) => state.greeting);
+
+  useEffect(() => {
+    dispatch(fetchGreeting());
+  }, [dispatch]);
+
+  console.log(greeting);
+  return (
+    <div>
+      <h1>{greeting}</h1>
+    </div>
+  );
+};
+
+export default Greeting;
